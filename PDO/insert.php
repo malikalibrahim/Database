c23ed3d1afbb1945829bcd537ab32559efd6ba79
Malik Al Ibrahim <133199700+Malik221948@users.noreply.github.com>
2023-06-03 04:04:16 +0200

    Update insert.php

--- PDO/insert.php
+++ PDO/insert.php
@@ -1,9 +1,10 @@
 <?php
-if(isset($_POST['submit'])) {
-$name = $_POST['product_naam'];
-$prijs = $_POST['prijs_per_stuk'];
-$omschrijving = $_POST['omschrijving'];
 
+if(isset($_POST['submit'])) {
+    $name = $_POST['product_naam'];
+    $prijs = $_POST['prijs_per_stuk'];
+    $omschrijving = $_POST['omschrijving'];
+    
 
 $host = 'localhost';
 $port = 3307;
@@ -12,15 +13,38 @@ $user = 'root';
 $pass = '';
 $charset = 'utf8mb4';
 
-$conn = new mysqli($host, $user, $pass, $db, $port, $charset);
-$sql = "INSERT INTO producten(product_naam,prijs_per_stuk,omschrijving) values ('$name', '$prijs', '$omschrijving')";
-mysqli_query($conn, $sql);
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port;";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try 
+{
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    echo "Connectie gemaakt!";
+} 
+catch (\PDOException $e) 
+{
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
+
+$query="INSERT INTO producten (product_naam, prijs_per_stuk,omschrijving) values (?, ?, ? )";
+$result=$pdo->prepare($query);
+$array_data = array($name , $prijs , $omschrijving);
+$result->execute($array_data);
+
+
+}
+
+
+
 ?>
-<form  method="POST">
 
 
-<label for="product_naam">productNaam:</label>
+<form  method="POST" action= 'select.php'>
+
+<label for="product_naam" >productNaam:</label>
 <input type="text" name="product_naam" required> <br>
 
 <label for="prijs_per_stuk">prijsPerStuk:</label>
@@ -28,6 +52,7 @@ mysqli_query($conn, $sql);
 
 <label for="omschrijving">Omschrijving:</label>
 <input type="text" name="omschrijving" required> <br>
+
 
 <input type="submit" name="submit">
 
